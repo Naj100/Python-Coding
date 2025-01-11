04cb52312857869134f8c7dda3986d37c916e5cc
Naj <122935899+Naj100@users.noreply.github.com>
2025-01-11 11:22:33 -0500

    Create python mario.py

---
+++ python mario.py
@@ -0,0 +1,10 @@
+from cs50 import get_int
+
+def main():
+    for i in range(n):
+        print("#")
+def get_height():
+    while True:
+        n = get_int("Height: ")
+        if n > 0:
+            break
